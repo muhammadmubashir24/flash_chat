936cbc052c9f521587e931a5a0e24c039d48fcf7
Muhammad Mubashir <muhammad.mubashir2498@gmail.com>
2025-02-07 07:59:43 +0500

    inital commit

---
+++ lib/main.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flash_chat/screens/chat_screen.dart';
+import 'package:flash_chat/screens/login_screen.dart';
+import 'package:flash_chat/screens/registration_screen.dart';
+import 'package:flash_chat/screens/welcome.dart';
+
+// void main() => runApp(FlashChat());
+void main() {
+  try {
+    runApp(const FlashChat());
+  } catch (e, stackTrace) {
+    debugPrint("Error: $e");
+    debugPrint("StackTrace: $stackTrace");
+  }
+}
+
+class FlashChat extends StatelessWidget {
+  const FlashChat({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark().copyWith(
+        textTheme: const TextTheme(
+          bodyMedium: TextStyle(color: Colors.black54),
+        ),
+      ),
+      initialRoute: 'welcome',
+      routes: {
+        'welcome': (context) => WelcomeScreen(),
+        'login_screen': (context) => LoginScreen(),
+        'registration_screen': (context) => RegistrationScreen(),
+        'chat_screen': (context) => ChatScreen(),
+      },
+    );
+  }
+}
